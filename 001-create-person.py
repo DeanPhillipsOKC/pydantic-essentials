1b790b986149e403858cc8edf5b662886ce86443
Dean Phillips <dephillips1977@gmail.com>
2024-09-20 21:13:16 -0500

    Basic example 1

---
+++ 001-create-person.py
@@ -0,0 +1,42 @@
+from pydantic import ValidationError
+from pydantic import BaseModel
+from utils import separator
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+
+    @property
+    def display_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+# Basic model instance creation
+p1 = Person(first_name="John", last_name="Doe", age=21)
+print(repr(p1))
+print(repr(p1.model_fields))
+separator()
+
+# Access properties
+print(f"Display Name: {p1.display_name}")
+separator()
+
+# Fails validation because all required fields are not passed in
+try:
+    Person(last_name="Doe")
+except ValidationError as ex:
+    print(ex)
+    separator()
+
+# Fails validation because age is a string instead of int
+try:
+    Person(first_name="John", last_name="Doe", age="twenty")
+except ValidationError as ex:
+    print(ex)
+    separator()
+
+# How to break encapsulation (oops)
+p1.age = "twenty"
+print(repr(p1))
+p1.age = 21 # change it back
+separator()
